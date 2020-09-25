2839b493740bb63c2a4c05bf7d33351845e6000d
Shreyas911 <shreyasg911@gmail.com>
2020-09-25 11:29:49 -0500

    factorial.sh checks if exactly 1 argument is provided on the command line

--- factorial.sh
+++ factorial.sh
@@ -7,4 +7,8 @@ while [ $i -gt 1 ]; do
 	i=`expr $i - 1`
 done
 
-echo "The factorial of the number $1 is $factorial"
+if [ $# = 1 ] ; then
+	echo "The factorial of the number $1 is $factorial"
+else
+	echo "ERROR: This script is supposed to find factorials. Please provide only one argument which is a whole number."
+fi	
