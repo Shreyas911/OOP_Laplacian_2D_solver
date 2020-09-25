432fa354980eae531682ae68f87ef80dafed4495
Shreyas911 <shreyasg911@gmail.com>
2020-09-25 11:32:43 -0500

    factorial.sh checks if the command line argument is a number.

--- factorial.sh
+++ factorial.sh
@@ -1,5 +1,8 @@
+# !/bin/bash
 factorial=1
 i=$1
+
+REGEX=^[0-9]+$
 
 ### This loop will not run for 0, so we directly have factorial = 1
 while [ $i -gt 1 ]; do
@@ -7,8 +10,8 @@ while [ $i -gt 1 ]; do
 	i=`expr $i - 1`
 done
 
-if [ $# = 1 ] ; then
+if [ $# = 1 ] && [[ $1 =~ $REGEX ]]; then
 	echo "The factorial of the number $1 is $factorial"
 else
 	echo "ERROR: This script is supposed to find factorials. Please provide only one argument which is a whole number."
-fi	
+fi
