1e12816b79d17a1edc6aeab474240145eb9cdc4c
Shreyas911 <shreyasg911@gmail.com>
2020-09-25 11:17:39 -0500

    add.sh now checks if both the inputs actually are integers using regex

--- add.sh
+++ add.sh
@@ -1,7 +1,7 @@
 # !/bin/bash
 REGEX=^[-]?[0-9]+$
 
-if [ $# = 2 ] && [[ $1 =~ $REGEX ]] ; then
+if [ $# = 2 ] && [[ $1 =~ $REGEX ]] && [[ "$2" =~ $REGEX ]] ; then
 	echo "The sum of the numbers $1 and $2 is $(( $1 + $2 ))"
 else
 	echo "ERROR: This script is supposed to add two integers, please provide two command line arguments that are integers."
