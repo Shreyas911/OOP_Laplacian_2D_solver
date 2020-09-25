a7db7324c6f6b9867c90e7eb93a47c3da0b2d102
Shreyas911 <shreyasg911@gmail.com>
2020-09-25 12:19:30 -0500

    tri.sh prints both pyramids out just fine now.

--- hw02/tri.sh
+++ hw02/tri.sh
@@ -10,5 +10,14 @@ for i in `seq 1 5`; do
 	done
 	echo 
 done
+for i in `seq 1 5`; do
+	for ((j = 1; j<=$((5-$i));j++)); do
+		echo -n " " 
+	done
+	for j in `seq $i`; do
+		echo -n ". "
+	done
+	echo 
+done
 
 
