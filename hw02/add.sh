1734a1c340824d17e0ec8d22a0ca1cd6ea464e46
Shreyas911 <shreyasg911@gmail.com>
2020-09-25 12:08:01 -0500

    add.sh moved to new directory

---
+++ hw02/add.sh
@@ -0,0 +1,9 @@
+# !/bin/bash
+
+REGEX=^[-+]?[0-9]+$
+
+if [ $# = 2 ] && [[ $1 =~ $REGEX ]] && [[ "$2" =~ $REGEX ]] ; then
+	echo "The sum of the numbers $1 and $2 is $(( $1 + $2 ))"
+else
+	echo "ERROR: This script is supposed to add two integers, please provide two command line arguments that are integers."
+fi
