224454728af2c6091f6c59d7956833a08941857b
Shreyas911 <shreyasg911@gmail.com>
2020-09-25 11:12:03 -0500

    add.sh now checks if two inputs were given.

--- add.sh
+++ add.sh
@@ -1,4 +1,8 @@
 # !/bin/bash
 
-echo "The sum of the numbers $1 and $2 is $(( $1 + $2 ))"
+if [ $# = 2 ] ; then
+	echo "The sum of the numbers $1 and $2 is $(( $1 + $2 ))"
+else
+	echo "ERROR: This script is supposed to add two integers, please provide two command line arguments that are integers."
+fi
 
