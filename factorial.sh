6f61392220f00a5c8393220e3deb3be0fbd95660
Shreyas911 <shreyasg911@gmail.com>
2020-09-25 11:54:16 -0500

    factorial.sh accounts for + sign before whole number inputs now.

--- factorial.sh
+++ factorial.sh
@@ -1,8 +1,11 @@
 # !/bin/bash
-factorial=1
-i=$1
 
-REGEX=^[0-9]+$
+REGEX=^[+]?[0-9]+$
+
+factorial=1
+
+### ignore + sign in input
+i=$(echo "$1"| grep -oE "[0-9]+$")
 
 ### This loop will not run for 0, so we directly have factorial = 1
 while [ $i -gt 1 ]; do
@@ -13,5 +16,5 @@ done
 if [ $# = 1 ] && [[ $1 =~ $REGEX ]]; then
 	echo "The factorial of the number $1 is $factorial"
 else
-	echo "ERROR: This script is supposed to find factorials. Please provide only one whole number argument."
+	echo "ERROR: This script is supposed to find factorials. Please provide only one argument which is a whole number."
 fi
