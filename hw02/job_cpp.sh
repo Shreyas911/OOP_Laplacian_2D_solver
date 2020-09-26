1ffa091d4c7079d3da341b91b3d98efd100addf7
Shreyas911 <shreyasg911@gmail.com>
2020-09-25 22:28:49 -0500

    Similar to job.sh, but for cpp files. The input from terminal is read into a file and fed to a.out.

---
+++ hw02/job_cpp.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -o job.log
+#SBATCH -e error.%j.out
+#SBATCH -J PI_ESTIMATION
+#SBATCH -p skx-normal
+#SBATCH -A cse38018
+#SBATCH -t 00:40:00
+
+#SBATCH --mail-user=shreyasg911@gmail.com
+#SBATCH --mail-type=begin
+
+TIMEFORMAT=%R ## Change time format to give only real time value, got this from stack overflow
+
+### Output stores stdout of ./pi.sh and stderr of time
+
+g++ pi.cpp -o ./a.out 
+echo 10 > input
+{ { time ./a.out >> pi.compiled.log;} < input ;} 2>> pi.compiled.log 
+echo 100 > input
+{ { time ./a.out >> pi.compiled.log;} < input ;} 2>> pi.compiled.log 
+echo 500 > input
+{ { time ./a.out >> pi.compiled.log;} < input ;} 2>> pi.compiled.log 
+echo 1000 > input
+{ { time ./a.out >> pi.compiled.log;} < input ;} 2>> pi.compiled.log 
+echo 5000 > input
+{ { time ./a.out >> pi.compiled.log;} < input ;} 2>> pi.compiled.log 
+echo 10000 > input
+{ { time ./a.out >> pi.compiled.log;} < input ;} 2>> pi.compiled.log
+echo 50000 > input
+{ { time ./a.out >> pi.compiled.log;} < input ;} 2>> pi.compiled.log  
