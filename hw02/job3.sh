bc50b4194ac61c02fcb0a779ccb2bbc6f89d9ae1
Shreyas911 <shreyasg911@gmail.com>
2020-09-27 17:17:58 -0500

    Using launcher now

--- hw02/job3.sh
+++ hw02/job3.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 #SBATCH -N 1
-#SBATCH -n 1
+#SBATCH -n 48
 #SBATCH -o job.log
 #SBATCH -e error.%j.out
-#SBATCH -J PI_ESTIMATION_CPP
+#SBATCH -J PI_PARALLEL
 #SBATCH -p skx-normal
 #SBATCH -A cse38018
 #SBATCH -t 00:40:00
 #SBATCH --mail-user=shreyasg911@gmail.com
 #SBATCH --mail-type=begin
+
+
+#setup launcher environment
+module purge
+module load TACC
+module load launcher 
 export LAUNCHER_SCHED=interleaved
+export LAUNCHER_PLUGIN_DIR=$LAUNCHER_DIR/plugins
+export LAUNCHER_RMI=SLURM
+
+
 TIMEFORMAT=%R ## Change time format to give only real time value, got this from stack overflow
 
-erel=1
-### This will run on all 48 cores
-{ time ./a.out >> iter.log;} 2>> iter.log
+#define the file with the commands to run in parallel
+export LAUNCHER_JOB_FILE=./jobfile
 
+#run the launcher
+$LAUNCHER_DIR/paramrun
