dfbd255b88175cb26e3de80963badd1008fb77b4
Shreyas911 <shreyasg911@gmail.com>
2020-09-27 16:08:44 -0500

    modified pi.cpp for use in last question.

---
+++ hw02/pi3.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>      /* printf, NULL */
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
+#include <iostream>
+#include <math.h>
+
+#define PI 3.14159265358979323846
+
+using namespace std;
+
+int main ()
+{
+	srand(time(NULL)); 
+	int N;
+	cin>>N;
+	double x,y,d; 
+	int counter = 0;
+	double piestimate,erel;
+
+	counter = 0;
+	for (int j = 0;j < N; j++){
+		x = double(rand())/double(RAND_MAX);
+		y = double(rand())/double(RAND_MAX);
+		d = x*x + y*y;
+		if(d<=1.0){
+			counter++;
+		}	
+	piestimate = double(4*counter)/double(N);
+	}
+	cout<<piestimate;
+}
