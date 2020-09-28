ff04e1b01d688f60d44cbeae7058249dd775a649
Shreyas911 <shreyasg911@gmail.com>
2020-09-28 13:30:08 -0500

    Added comments.

--- hw02/pi.cpp
+++ hw02/pi.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <math.h>
 
+// Actual value of PI
 #define PI 3.14159265358979323846
 
 using namespace std;
@@ -19,14 +20,18 @@ int main ()
 
 	counter = 0;
 	for (int j = 0;j < N; j++){
+		// Generate x and y between 0 and 1
 		x = double(rand())/double(RAND_MAX);
 		y = double(rand())/double(RAND_MAX);
 		d = x*x + y*y;
+		// If inside circle, increase counter
 		if(d<=1.0){
 			counter++;
 		}	
+	//estimate of PI and erel
 	piestimate = double(4*counter)/double(N);
 	erel = fabs(piestimate - PI)/PI;
 	}
+	//output in the required format
 	cout<<N<<" "<<counter<<" "<<N-counter<<" "<<piestimate<<" "<<erel<<" ";
 }
