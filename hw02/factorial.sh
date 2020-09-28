ab15c16517dd9d7afacd578873c6d4ab7a59d447
Shreyas911 <shreyasg911@gmail.com>
2020-09-28 11:34:39 -0500

    1.0 or 1.0000 is now treated as 1

--- hw02/factorial.sh
+++ hw02/factorial.sh
@@ -1,14 +1,15 @@
 # !/bin/bash
 
 ### To check if we have whole numbers as input
-REGEX=^[+]?[0-9]+$
+REGEX=^[+]?[0-9]+[\.]?0*$
 
 ### Initialize factorial
 factorial=1
 
-### ignore + sign befor the numbers in input
-i=$(echo "$1"| grep -oE "[0-9]+$")
+### ignore + sign before the numbers and the decimal point after in input
+i=$(echo "$1"| grep -oE "[0-9]+" | head -1)
 
+### Loop only if the input was a valid whole number
 if [ $# = 1 ] && [[ $1 =~ $REGEX ]]; then
 
 	### This loop will not run for 0, so we directly have factorial = 1
