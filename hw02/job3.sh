cfb9c8d46051aeed1baf4f450f952bc6b53df3e5
Shreyas911 <shreyasg911@gmail.com>
2020-09-27 20:00:01 -0500

    Minor corrections

--- hw02/job3.sh
+++ hw02/job3.sh
@@ -8,7 +8,7 @@
 #SBATCH -A cse38018
 #SBATCH -t 00:05:00
 
-
+PI=$(echo "scale =10; 3.14159265358979323846" | bc -l)
 #setup launcher environment
 module purge
 module load TACC
@@ -17,6 +17,10 @@ export LAUNCHER_SCHED=interleaved
 export LAUNCHER_PLUGIN_DIR=$LAUNCHER_DIR/plugins
 export LAUNCHER_RMI=SLURM
 
+if [ -e iter.log ]; then
+  rm iter.log
+fi
+touch iter.log
 
 TIMEFORMAT=%R ## Change time format to give only real time value, got this from stack overflow
 
@@ -25,3 +29,18 @@ export LAUNCHER_JOB_FILE=./jobfile
 
 #run the launcher
 $LAUNCHER_DIR/paramrun
+
+epsilon=$(echo "scale=10; 0.000005" | bc -l)
+erel=1
+iter=0
+pi_average=0
+N_i_average=0
+pi_iter_value=0
+while [[ $(echo "scale=10; $erel > $epsilon" | bc -l) ]];do
+	iter=$(($iter+1))	
+	awk '{pi_iter_value = pi_iter_value + $4;}END{pi_iter_value = pi_iter_value/48.0;}' iter.log
+	pi_average=$(echo "scale=10; ($pi_average*($iter-1)+$pi_iter_value)/$iter" | bc -l)
+	erel=$(echo "scale=10; sqrt((($pi_average-$PI)/$PI)^2)" | bc -l)
+done
+
+echo $iter
