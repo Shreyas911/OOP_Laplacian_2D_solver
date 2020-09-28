e3fec269af4fb7ed349df5de60d885abd42a5b80
Shreyas911 <shreyasg911@gmail.com>
2020-09-28 11:15:52 -0500

    Shifted while loop into the if statement checking for valid inputs.

--- hw02/factorial.sh
+++ hw02/factorial.sh
@@ -1,20 +1,24 @@
 # !/bin/bash
 
+### To check if we have whole numbers as input
 REGEX=^[+]?[0-9]+$
 
+### Initialize factorial
 factorial=1
 
-### ignore + sign in input
+### ignore + sign befor the numbers in input
 i=$(echo "$1"| grep -oE "[0-9]+$")
 
-### This loop will not run for 0, so we directly have factorial = 1
-while [ $i -gt 1 ]; do
-	factorial=`expr $factorial \* $i`
-	i=`expr $i - 1`
-done
-
 if [ $# = 1 ] && [[ $1 =~ $REGEX ]]; then
+
+	### This loop will not run for 0, so we directly have factorial = 1
+	while [ $i -gt 1 ]; do
+		factorial=`expr $factorial \* $i`
+		i=`expr $i - 1`
+	done
+
 	echo "The factorial of the number $1 is $factorial"
+
 else
 	echo "ERROR: This script is supposed to find factorials. Please provide only one argument which is a whole number."
 fi
