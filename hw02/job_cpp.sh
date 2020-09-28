b94480db22742c70ed3e481da303eeda18e8ece6
Shreyas911 <shreyasg911@gmail.com>
2020-09-28 13:35:44 -0500

    No longer getting error output since it is not useful anymore.

--- hw02/job_cpp.sh
+++ hw02/job_cpp.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH -o job.log
-#SBATCH -e error.%j.out
+#SBATCH -o job_cpp_results.log
+##SBATCH -e error.%j.out
 #SBATCH -J PI_ESTIMATION_CPP
 #SBATCH -p skx-normal
 #SBATCH -A cse38018
