0a5a1d1231c1145dc42178facb099b68a18bcf92
Shreyas911 <shreyasg911@gmail.com>
2020-09-28 13:31:46 -0500

    Replace variable counter by N_i

--- hw02/pi.cpp
+++ hw02/pi.cpp
@@ -14,24 +14,25 @@ int main ()
 	srand(time(NULL)); 
 	int N;
 	cin>>N;
-	double x,y,d; 
-	int counter = 0;
+	double x,y,d;
+	//Counter for number of points inside circle. 
+	int N_i = 0; 
 	double piestimate,erel;
 
-	counter = 0;
+	N_i = 0;
 	for (int j = 0;j < N; j++){
 		// Generate x and y between 0 and 1
 		x = double(rand())/double(RAND_MAX);
 		y = double(rand())/double(RAND_MAX);
 		d = x*x + y*y;
-		// If inside circle, increase counter
+		// If inside circle, increase N_i
 		if(d<=1.0){
-			counter++;
+			N_i++;
 		}	
 	//estimate of PI and erel
-	piestimate = double(4*counter)/double(N);
+	piestimate = double(4*N_i)/double(N);
 	erel = fabs(piestimate - PI)/PI;
 	}
 	//output in the required format
-	cout<<N<<" "<<counter<<" "<<N-counter<<" "<<piestimate<<" "<<erel<<" ";
+	cout<<N<<" "<<N_i<<" "<<N-N_i<<" "<<piestimate<<" "<<erel<<" ";
 }
