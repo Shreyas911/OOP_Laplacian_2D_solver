71ac5c78a611eb8c5ad500a27dee2aac12d0b04b
Shreyas911 <shreyasg911@gmail.com>
2020-09-28 13:04:10 -0500

    Added comments.

--- hw02/pi.sh
+++ hw02/pi.sh
@@ -1,18 +1,28 @@
 # !/bin/bash
+
+### Total number of points
 N=$1
+
+### Actual value of PI
 PI=$(echo "scale =10; 3.14159265358979323846" | bc -l)
+
+### Count points inside the circle
 COUNTER=0
 
 for i in `seq $N`;do
+	### Randomly generate x and y between 0 and 1.
 	x=$(echo "scale=10; $RANDOM/32767" | bc -l)
 	y=$(echo "scale=10; $RANDOM/32767" | bc -l)
 	d1=$(echo "scale=10; $x * $x" | bc -l)
 	d2=$(echo "scale=10; $y * $y" | bc -l)
 	d=$(echo "scale=10; $d1 + $d2" | bc -l)
+	### If inside cicle, increase COUNTER
 	if [[ $(echo "scale = 10; $d <= 1.0 " | bc -l) = 1 ]]; then
 	 	COUNTER=$(( $COUNTER+1 ))
 	fi
 	piestimate=$(echo "scale=10; 4*$COUNTER/${N[n]}" | bc -l)
 	erel=$(echo "scale=10; sqrt((($PI-$piestimate)/$PI)^2)" | bc -l)
 done
+
+### Printing in the required format, the time appending is done by job.sh
 echo -n "$N $COUNTER $(($N-$COUNTER)) $piestimate $erel "
