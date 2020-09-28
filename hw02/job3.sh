5ddbd8d1838190721789064f14ec0d6cff313b31
Shreyas911 <shreyasg911@gmail.com>
2020-09-27 23:51:02 -0500

    job3.sh that works pretty well, a few issues though with breaking the while loop.

--- hw02/job3.sh
+++ hw02/job3.sh
@@ -6,7 +6,7 @@
 #SBATCH -J PI_PARALLEL
 #SBATCH -p skx-normal
 #SBATCH -A cse38018
-#SBATCH -t 00:05:00
+#SBATCH -t 00:10:00
 
 PI=$(echo "scale =10; 3.14159265358979323846" | bc -l)
 #setup launcher environment
@@ -16,6 +16,8 @@ module load launcher
 export LAUNCHER_SCHED=interleaved
 export LAUNCHER_PLUGIN_DIR=$LAUNCHER_DIR/plugins
 export LAUNCHER_RMI=SLURM
+#define the file with the commands to run in parallel
+export LAUNCHER_JOB_FILE=./jobfile
 
 if [ -e iter.log ]; then
   rm iter.log
@@ -24,23 +26,24 @@ touch iter.log
 
 TIMEFORMAT=%R ## Change time format to give only real time value, got this from stack overflow
 
-#define the file with the commands to run in parallel
-export LAUNCHER_JOB_FILE=./jobfile
-
-#run the launcher
-$LAUNCHER_DIR/paramrun
-
 epsilon=$(echo "scale=10; 0.000005" | bc -l)
 erel=1
 iter=0
 pi_average=0
 N_i_average=0
 pi_iter_value=0
+
 while [[ $(echo "scale=10; $erel > $epsilon" | bc -l) ]];do
-	iter=$(($iter+1))	
-	awk '{pi_iter_value = pi_iter_value + $4;}END{pi_iter_value = pi_iter_value/48.0;}' iter.log
-	pi_average=$(echo "scale=10; ($pi_average*($iter-1)+$pi_iter_value)/$iter" | bc -l)
-	erel=$(echo "scale=10; sqrt((($pi_average-$PI)/$PI)^2)" | bc -l)
+	#empty the iter.log file
+	> iter.log
+	#run the launcher
+	$LAUNCHER_DIR/paramrun
+
+        iter=$(($iter+1))
+        pi_iter_value=$(awk 'BEGIN{temp=0;}{temp = temp + $4;}END{temp = temp/48.0;print temp;}' iter.log| bc -l)
+        pi_average=$(echo "scale=10; ($pi_average*($iter-1)+$pi_iter_value)/$iter" | bc -l)
+        erel=$(echo "scale=20; sqrt((($pi_average-$PI)/$PI)^2)" | bc -l)
+	echo "$iter $pi_iter_value $pi_average $erel">>temp1
+	$(echo "scale=10; $erel > $epsilon" | bc -l)>>temp1
 done
 
-echo $iter
