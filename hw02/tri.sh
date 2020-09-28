5c9a4134281218e18ca1eabc0b4c970e0a0a4fce
Shreyas911 <shreyasg911@gmail.com>
2020-09-28 12:22:57 -0500

    Changed the style of for loops to more of the C++ one.

--- hw02/tri.sh
+++ hw02/tri.sh
@@ -1,21 +1,21 @@
 # !/bin/bash
 
 # Print the pyramid of numbers
-for i in `seq 1 5`; do
+for ((i = 1; i<=5;i++)); do
 	for ((j = 1; j<=$((5-$i));j++)); do
 		echo -n " " 
 	done
-	for j in `seq $i`; do
+	for  ((j = 1; j<=$i;j++)); do
 		echo -n "$i "
 	done
 	echo 
 done
 # Print the pyramid of dots
-for i in `seq 1 5`; do
+for ((i = 1; i<=5;i++)); do
 	for ((j = 1; j<=$((5-$i));j++)); do
 		echo -n " " 
 	done
-	for j in `seq $i`; do
+	for  ((j = 1; j<=$i;j++)); do
 		echo -n ". "
 	done
 	echo 
