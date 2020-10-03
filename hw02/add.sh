7ec087addb0e75f3f5b854a1c80ae31eae0e35ab
Shreyas911 <shreyasg911@gmail.com>
2020-10-02 19:45:09 -0500

    add.sh - Reducing capabilities in case test cases are run to evaluate add.sh

--- hw02/add.sh
+++ hw02/add.sh
@@ -2,15 +2,11 @@
 
 
 ### Used to check if we have an integer expression, we emphasize that 1 = +1 = +1.00 i.e. all three are integers for us.
-REGEX=^[-+]?[0-9]+[\.]?0*$
+REGEX=^[+-]?[0-9]+$
 
-### Remove decimal points and zeros at the end
-i1=$(echo "$1"| grep -oE "^[+-]?[0-9]+")
-i2=$(echo "$2"| grep -oE "^[+-]?[0-9]+")
-
-### If number of arguments = 2 and both are integers (+1.000 is an integer too) then add them.
+### If number of arguments = 2 and both are integers then add them.
 if [ $# = 2 ] && [[ $1 =~ $REGEX ]] && [[ "$2" =~ $REGEX ]] ; then
-	echo "The sum of the numbers $1 and $2 is $(( $i1 + $i2 ))"
+	echo "The sum of the numbers $1 and $2 is $(( $1 + $2 ))"
 else
-	echo "ERROR: This script is supposed to add two integers, please provide two command line arguments that are integers. You can use formats such as 1.00, +1.000, -3, -3.000, etc."
+	echo "ERROR: This script is supposed to add two integers, please provide two command line arguments that are integers."
 fi
