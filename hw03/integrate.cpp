392baf8e0097768ace7375e49dcc5b149e309bee
Shreyas911 <shreyasg911@gmail.com>
2020-10-07 00:18:11 -0500

    Can take command line input now, input verification added.

--- hw03/integrate.cpp
+++ hw03/integrate.cpp
@@ -3,9 +3,32 @@
 #include "trapezoidal.h"
 #include "simpson.h"
 
+#define erf_tab 0.84270079295
+
 using namespace std;
 
-int main(){
-	cout << integral_trapezoidal(1,100000) << endl;
-	cout << integral_simpson(1,100000) << endl;
+int main(int argc, char *argv[]) {
+	// Input verification
+	// atoi returns integer part of input and 0 if not a numeric input
+	if (argc != 3 || atoi(argv[2]) <= 0 || (atoi(argv[1]) != 1 & atoi(argv[1]) != 2)) {
+		cerr << "ERROR: First input should be 1 or 2 for Trapezoidal and Simpson's rule respectively. The second input should be a positive integer for the number of points" << endl;
+	return 1;
+	}
+	
+	int N = atoi(argv[2]);
+
+	if (atoi(argv[1]) == 1){
+
+		double integral = integral_trapezoidal(1, N);
+		double erel = abs(integral - erf_tab)/integral;
+
+		cout << integral << " " << erel << endl;
+	}       
+	else{
+
+                double integral = integral_simpson(1, N);
+                double erel = abs(integral - erf_tab)/integral;
+
+                cout << integral << " " << erel << endl;
+        }                       
 }
