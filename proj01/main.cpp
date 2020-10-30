ce58c5435402397ec6ba69f57b57112e4250936b
Shreyas911 <shreyasg911@gmail.com>
2020-10-29 23:11:29 -0700

    Solvers added with makefile and a basic main function. Dynamic memory allocations used.

---
+++ proj01/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include "solvers.h"
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+	const int n = 100;
+
+	double** A = new double*[n]; // each element is a pointer to an array.
+	double* q = new double[n];
+	double* T = new double[n]; 
+
+	for(int i = 0; i < n; i++){
+	    A[i] = new double[n]; // build rows
+	}
+
+	for(int i = 0; i < n; i++){
+	    q[i] = i;		
+		for(int j = 0; j < n; j++){
+			if (i<=j) A[i][j] = i+j+1;
+			else A[i][j] = 0; 
+	    }
+	}
+
+	T = jacobi(n, A, q, 0.001, 100);
+	T = gauss(n, A, q, 0.001, 100);
+	for(int i = 0; i < n; i++){
+    	delete[] A[i];
+    	cout<<T[i]<<endl;
+	}
+
+delete[] A;
+delete[] q;
+delete[] T;
+}	
+
