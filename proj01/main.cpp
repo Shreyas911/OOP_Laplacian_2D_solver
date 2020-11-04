b2fef2621abf25c2b120a69176f5fb9072e9b994
Shreyas911 <shreyasg911@gmail.com>
2020-11-04 01:05:57 -0600

    Autotools now operational annd basic MASA calls added to main which will have to be made modular i.e. separate functions

--- proj01/main.cpp
+++ proj01/main.cpp
@@ -1,22 +1,34 @@
 #include <iostream>
 #include "solvers.h"
 #include "assemble.h"
+#include <masa.h>
 
 using namespace std;
+using namespace MASA; 
 
 int main(int argc, char *argv[]) {
+
+	masa_init("laplace 2D exact","laplace_2d");
+	masa_display_param();
+	masa_set_param("Lx",1.);
+	masa_set_param("Ly",masa_get_param("Lx"));
 
 	const int n = 7;
 	const int nn = n*n;
 	const int MAX_ITERS = 100;
 	const double TOL = 0.001;
-	
-	double** A = order4_dim2(n); // each element is a pointer to an array.
+	const double L = masa_get_param("Lx");
+	const double h = L/(n-1);
+	double** A = matrix_order4_dim2(n); // each element is a pointer to an array.
 	double* q = new double[nn];
-	double* T = new double[nn]; 
-
-	for(int i = 0; i < nn; i++){
-	    q[i] = i;
+	double* T = new double[nn];
+	double* T_exact = new double[nn];
+	
+	for(int k = 0; k < nn; k++){
+		int i = k % n;
+		int j = k / n;
+		q[k] = masa_eval_2d_source_f(i*h,j*h);
+		T_exact[k] = masa_eval_2d_exact_phi(i*h,j*h);
 	}
 
 	T = jacobi(n, A, q, TOL, MAX_ITERS);
