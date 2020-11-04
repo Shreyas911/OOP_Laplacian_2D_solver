88398c8dba8f7852a3f02cdd35062446ce6ab1fe
Shreyas911 <shreyasg911@gmail.com>
2020-11-04 17:39:05 -0600

    Ignore makefile

--- proj01/matrix_assemble.h
+++
@@ -1,4 +0,0 @@
-double ** matrix_order2_dim1(int n);
-double ** matrix_order4_dim1(int n);
-double ** matrix_order2_dim2(int n);
-double ** matrix_order4_dim2(int n);
--- proj01/solvers.h
+++
@@ -1,3 +0,0 @@
-double l2_norm(int n, double *x);
-double *jacobi(int n, double** A, double *q, double TOL, unsigned int MAX_ITERS);
-double *gauss(int n, double** A, double *q, double TOL, unsigned int MAX_ITERS);
