2e6bc5c48594e1168ec6c91cc7d230eb7e329d99
Shreyas911 <shreyasg911@gmail.com>
2020-11-05 00:16:41 -0600

    Some minor change

--- proj01/build_autotools.sh
+++ proj01/build_autotools.sh
@@ -1,3 +1,3 @@
 autoreconf --install
 export PKGPATH=/work/00161/karl/stampede2/public
-./configure --with-masa=$PKGPATH/masa-gnu7-0.50 \--with-grvy=$PKGPATH/grvy-gnu7-0.34
+./configure --with-masa=$PKGPATH/masa-gnu7-0.50 --with-grvy=$PKGPATH/grvy-gnu7-0.34
