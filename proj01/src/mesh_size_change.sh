4543593050220da2c6dc8b02ac3c6657c6f28447
Shreyas911 <shreyasg911@gmail.com>
2020-11-12 20:05:38 -0600

    This script runs the executable in all configurations and stores outputs to unique files.

--- proj01/src/mesh_size_change.sh
+++ proj01/src/mesh_size_change.sh
@@ -1,18 +1,35 @@
 #!/bin/bash
 
->temp.tmp
->output.tmp
+declare -a n=(10 20 40)
+declare -a solvers=("gauss" "jacobi")
+declare -a dimensions=(1)
+declare -a orders=(2 4)
+rm output*.tmp
 
-awk '{if($1 ~ /grid_points/){$3 = 10;} print $0;}' input.dat > input.tmp && mv input.tmp input.dat
-./heat_solve >> temp.tmp
-awk '{if($1 ~ /grid_points/){$3 = 20;} print $0;}' input.dat > input.tmp && mv input.tmp input.dat
-./heat_solve >> temp.tmp
-awk '{if($1 ~ /grid_points/){$3 = 30;} print $0;}' input.dat > input.tmp && mv input.tmp input.dat
-./heat_solve >> temp.tmp
-awk '{if($1 ~ /grid_points/){$3 = 50;} print $0;}' input.dat > input.tmp && mv input.tmp input.dat
-./heat_solve >> temp.tmp
+for order in "${orders[@]}"
+do
+	awk -v order=$order '{if($1 ~ /order/){$3 = order;} print $0;}' input.dat > input.tmp && mv input.tmp input.dat
+	for solver in "${solvers[@]}"
+	do
 
-echo "n       L2_error          Time taken" >> output.tmp
-awk '{if($1 == "L2"){printf("%s %s ", $7, $9);}if($1 == "Total"){print $5;}}' temp.tmp >> output.tmp
-#grep "Total Measured Time" temp.tmp | awk '{print $5}' >> output.tmp
+		awk -v solver=$solver '{if($1 ~ /solver_name/){$3 = solver;} print $0;}' input.dat > input.tmp && mv input.tmp input.dat
 
+		for dimension in "${dimensions[@]}"
+		do
+		
+			awk -v dimension=$dimension '{if($1 ~ /dimension/){$3 = dimension;} print $0;}' input.dat > input.tmp && mv input.tmp input.dat
+			>temp.tmp
+			> output_${solver}_dim${dimension}_order${order}.tmp	
+			for i in "${n[@]}"
+			do 
+				awk -v num_points=$i '{if($1 ~ /grid_points/){$3 = num_points;} print $0;}' input.dat > input.tmp && mv input.tmp input.dat
+				./heat_solve >> temp.tmp
+			done
+
+			echo "n       L2_error          Time taken" >> output_${solver}_dim${dimension}_order${order}.tmp
+			awk '{if($1 == "L2"){printf("%s %s ", $7, $9);}if($1 == "Total"){print $5;}}' temp.tmp >> output_${solver}_dim${dimension}_order${order}.tmp
+
+			rm temp.tmp
+		done
+	done
+done
