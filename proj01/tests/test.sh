d91068a922e5cde4b549f51c2d090a967ede0228
Shreyas911 <shreyasg911@gmail.com>
2020-11-17 19:22:56 -0800

    Added comments

--- proj01/tests/test.sh
+++ proj01/tests/test.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bats
 
-@test "addition using bc" {
-	result="$(echo 2+2 | bc)"
-	[ "$result" -eq 4 ]
-}
-
+### Basic test that the code is working and is in verification mode 
 @test "verify that the code is working" {
 	
 	run ./tests [ "$status" -eq 0 ]
 	[ "${lines[0]}" = "--> verification_mode = 1" ]
 }
+
+### Test that all configurations give the same output as their reference output files
+
