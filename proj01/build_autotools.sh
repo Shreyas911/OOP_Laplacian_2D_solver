2e4a9f997d5c9b1a1b3768de53cf47a59f3c25b4
Shreyas911 <shreyasg911@gmail.com>
2020-11-20 01:55:11 -0600

    Minor change, if any.

--- proj01/build_autotools.sh
+++ proj01/build_autotools.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# Add to path variable for make check - export  PATH=/work/00161/karl/stampede2/public/bats/bin/:$PATH
 autoreconf -f -i
 export PKGPATH=/work/00161/karl/stampede2/public
 ./configure --with-masa=$PKGPATH/masa-gnu7-0.50 --with-grvy=$PKGPATH/grvy-gnu7-0.34
