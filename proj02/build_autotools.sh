b11052a85984da65f769c5fc63ea5f1c8f42581b
Shreyas911 <shreyasg911@gmail.com>
2020-12-01 00:48:09 -0600

    lcov added but not working yet

--- proj02/build_autotools.sh
+++ proj02/build_autotools.sh
@@ -2,4 +2,4 @@
 # Add to path variable for make check - export  PATH=/work/00161/karl/stampede2/public/bats/bin/:$PATH
 autoreconf -f -i
 export PKGPATH=/work/00161/karl/stampede2/public
-./configure --with-masa=$PKGPATH/masa-gnu7-0.50 --with-grvy=$PKGPATH/grvy-gnu7-0.34
+./configure --with-masa=$PKGPATH/masa-gnu7-0.50 --with-grvy=$PKGPATH/grvy-gnu7-0.34 --enable-coverage
