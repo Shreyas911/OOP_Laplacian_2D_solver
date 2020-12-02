fe50779b4d03dd2cb5ec7dfade06eae0ecdc083d
Shreyas911 <shreyasg911@gmail.com>
2020-12-01 18:01:19 -0600

    Added HDF5 to bootstrappinng

--- proj02/build_autotools.sh
+++ proj02/build_autotools.sh
@@ -2,4 +2,4 @@
 # Add to path variable for make check - export  PATH=/work/00161/karl/stampede2/public/bats/bin/:$PATH
 autoreconf -f -i
 export PKGPATH=/work/00161/karl/stampede2/public
-./configure --with-masa=$PKGPATH/masa-gnu7-0.50 --with-grvy=$PKGPATH/grvy-gnu7-0.34 --enable-coverage
+./configure --with-masa=$PKGPATH/masa-gnu7-0.50 --with-grvy=$PKGPATH/grvy-gnu7-0.34 --with-hdf5=$TACC_HDF5_DIR --enable-coverage
