4ee7c1923076341bb4ef9cd8208440c52dcf68ab
Shreyas911 <shreyasg911@gmail.com>
2020-12-08 00:58:14 -0600

    More petsc stuff

--- proj02/build_autotools.sh
+++ proj02/build_autotools.sh
@@ -2,4 +2,5 @@
 # Add to path variable for make check - export  PATH=/work/00161/karl/stampede2/public/bats/bin/:$PATH
 autoreconf -f -i
 export PKGPATH=/work/00161/karl/stampede2/public
-./configure --with-masa=$PKGPATH/masa-gnu7-0.50 --with-grvy=$PKGPATH/grvy-gnu7-0.34 --with-hdf5=$TACC_HDF5_DIR --enable-coverage
+./configure CXX=mpicxx --with-masa=$PKGPATH/masa-gnu7-0.50 --with-grvy=$PKGPATH/grvy-gnu7-0.34 --with-hdf5=$TACC_HDF5_DIR --with-petsc=$TACC_PETSC_DIR
+#./configure --with-masa=$PKGPATH/masa-gnu7-0.50 --with-grvy=$PKGPATH/grvy-gnu7-0.34 --with-hdf5=$TACC_HDF5_DIR --enable-coverage
